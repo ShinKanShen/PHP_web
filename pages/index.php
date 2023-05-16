de990510045eaf70d064071f0328f5c1ae8784f3
LuanNgo1807 <luantiti1807@gmail.com>
2023-05-16 16:53:19 +0700

    btl_php

---
+++ pages/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Homepage</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">		
+<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+
+<body>
+	<div class="wrapper">
+		<div class="header">
+			<div id="logo">
+				<a href="index.php">
+					<img src="../images/logo.png">
+				</a>
+			</div>
+			<div class="background-header">
+				<img src="../images/banner1.png" alt="ảnh đồ ăn">
+				<img src="../images/banner2.png" alt="ảnh đồ uống">
+				<img src="../images/banner3.png" alt="ảnh pizza">
+			</div>
+		</div>
+		<div class="clear1"></div>
+		<div class="menu">
+			<ul class="list-menu">
+				<li><a href="index.php">Trang chủ</a></li>
+				<li><a href="page1.php">Thực đơn</a>
+					<ul class="mini-menu1">
+						<li><a href="#">Ăn vặt</a></li>
+						<li><a href="#">Đồ uống</a></li>
+						<li><a href="#">Pizza</a></li>
+						<li><a href="#">Cơm chiên</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Tìm hiểu</a></li>
+				<li><a href="#">Khuyến mãi</a></li>
+				<li><a href="#">Tin tức</a></li>
+				<li><a href="#">Liên hệ</a>
+					<ul class="mini-menu2">
+						<li><a href="#">Facebook</a></li>
+						<li><a href="#">Instagram</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<div id="main">
+			<div class="sidebar">
+				<ul class="list-sidebar">
+					<li><a href="#">Ăn vặt</a></li>
+					<li><a href="#">Đồ uống</a></li>
+					<li><a href="#">Pizza</a></li>
+					<li><a href="#">Cơm chiên</a></li>
+					<li><a href="#">Khuyến mãi</a></li>
+				</ul>
+			</div>
+			<div class="maincontent">
+				<div id="bestseller">
+					<a href="#">
+						<img src="../images/best_seller.png" alt="bestseller">
+					</a>
+				</div>
+				<div class="combo">
+					<a href="#">
+						<img src="..\images\combo1.png" alt="combo1">
+  						<img src="..\images\combo2.png" alt="combo2">
+					</a>
+				</div>
+				<div class="combo1">
+					<a href="#">
+						<img src="..\images\sale1.png" alt="combo1">
+  						<img src="..\images\sale2.png" alt="combo2">
+					</a>
+				</div>
+			</div>
+			</div>
+			<div class="clear"></div>
+			<div class="pre-footer">
+				<img src="../images/pre-footer.png" alt="pre-footer">
+			</div>
+			<div class="footer">
+				
+			</div>
+	
+</body>
+
+</html>
